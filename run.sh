2233859b32893019238012f769bc21f76e4f45f5
arnoldas kurbanovas <akurbanovas@albany.edu>
2017-09-27 17:03:26 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,16 @@
-#!/bin/bash  
-echo "Starting raspPy framework download"
-conda install git https://github.com/ASRCsoft/raspPy.git
-echo "Starting gcc and gfortran download"  
-conda install gcc
-echo "Starting xarray package download" 
-conda install -c anaconda xarray=0.9.5
-echo "Done! You are ready to run the raspPY framework"  
+#!/bin/bash                                                                                                                         
+                                                                                                                                    
+echo "Starting gcc and gfortran download"                                                                                           
+conda install gcc                                                                                                                   
+echo "Starting xarray package download"                                                                                             
+conda install -c anaconda xarray=0.9.5                                                                                              
+                                                                                                                                    
+echo "Starting raspPy framework download"                                                                                           
+pip3 install git+https://github.com/ASRCsoft/raspPy.git                                                                             
+                                                                                                                                    
+echo "upgrading raspPy packages"                                                                                                    
+pip3 install git+https://github.com/ASRCsoft/raspPy.git --upgrade                                                                   
+echo "starting pull of raspPy jupyter notebook"                                                                                     
+git clone https://github.com/ASRCsoft/xcite_notebooks.git                                                                           
+                                                                                                                                    
+echo "Done! You are ready to run the raspPY framework"
