bc32aed6df4cb083223ce8f32129092178b9ea82
arnoldas kurbanovas <akurbanovas@albany.edu>
2017-09-27 17:38:38 -0400

    added statsmodels package download

--- raspPybash.sh
+++ raspPybash.sh
@@ -1,11 +1,18 @@
-Creating a bash script to import raspPy
-#!/bin/bash
-Then save the file with .sh extension. Next mark it executable using chmod +x file
-#!/bin/bash  
-echo "Starting raspPy framework download"
-conda install git https://github.com/ASRCsoft/raspPy.git
-echo "Starting gcc and gfortran download"  
-conda install gcc
-echo "Starting xarray package download" 
-conda install -c anaconda xarray=0.9.5
-echo "Done! You are ready to run the raspPY framework"  
+#!/bin/bash                                                                                                                                                                                                                                                             
+echo "Starting gcc and gfortran download"                                                                                           
+conda install gcc                                                                                                                   
+echo "Starting xarray package download"                                                                                             
+conda install -c anaconda xarray=0.9.5                                                                                                                                                                                                                                
+echo "Starting raspPy framework download"                                                                                           
+pip install git+https://github.com/ASRCsoft/raspPy.git                                                                                                                                                                                                               
+echo "upgrading raspPy packages"                                                                                                    
+#pip install git+https://github.com/ASRCsoft/raspPy.git --upgrade                                                                   
+echo "starting pull of raspPy jupyter notebook"                                                                                     
+git clone https://github.com/ASRCsoft/xcite_notebooks.git
+echo "Starting statsmodels package download"
+pip install statsmodels                                                                                                                                                                                                        
+echo "Done! You are ready to run the raspPY framework"
+
+#NOTE:
+#save the file with .sh extension. Next mark it executable using chmod +x fileName
+#Then run it using ./fileName.sh
